7dbc429dd1216fb7d094e1f66b9dda8fa8db1098
rich-hart <rich.hart@rice.edu>
2016-07-20 16:15:17 -0500

    remove comments

--- api/tests.py
+++ api/tests.py
@@ -1,19 +1,14 @@
 from django.contrib.auth.models import AnonymousUser, User
 from django.test import TestCase
-
-
 from rest_framework.test import APIRequestFactory as RequestFactory
 
-# Using the standard RequestFactory API to create a form POST request
 class SimpleTest(TestCase):
     def setUp(self):
-        # Every test needs access to the request factory.
         self.factory = RequestFactory()
         self.user = AnonymousUser() 
-    def test_proofs(self):
-        # Create an instance of a GET request.
-        request = self.factory.get('/api/proofs')
 
+    def test_proofs(self):
+        request = self.factory.get('/api/proofs')
         request.user = self.user
 
 
