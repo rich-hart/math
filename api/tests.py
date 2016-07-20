f31b9d9366fa827f180c1abbc7d95d9df35888b0
rich-hart <rich.hart@rice.edu>
2016-07-20 17:47:19 -0500

    add notes for post file test

--- api/tests.py
+++ api/tests.py
@@ -27,3 +27,24 @@ class ProofAPITest(TestCase):
         expected['diagram']=None 
         returned = json.loads(response.content.decode(response.charset))
         self.assertEqual(expected, returned)
+
+#    def test_post_with_file(self):
+#        request = self.factory.get('/api/proofs/', format='json', follow=True)
+#        request.user = self.user
+#        with open('api/test_image.jpeg') as fp:
+#            data = {   
+#                       "title": "title",
+#                       "prove": "prove",
+#                       "diagram": fp, 
+#                       "given": "given",
+#                       "plan": "plan",
+#                       "statements": [],
+#                       "reasons": [],
+#                   }
+#            response = self.client.post('/api/proofs/',data)
+#            self.assertEqual(response.status_code, 201)
+#            expected = data
+#            expected['id'] = 1
+#            expected['diagram']=None 
+#            returned = json.loads(response.content.decode(response.charset))
+#            self.assertEqual(expected, returned)
