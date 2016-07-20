21614c21da8678c6d7c5a219bb2a919ef9d0eafb
rich-hart <rich.hart@rice.edu>
2016-07-20 17:27:47 -0500

    update tests

--- api/tests.py
+++ api/tests.py
@@ -4,28 +4,26 @@ from rest_framework.test import APIRequestFactory as RequestFactory
 import json
 
 from .views import ProofViewSet
-class SimpleTest(TestCase):
+class ProofAPITest(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = AnonymousUser() 
 
-    def test_proofs(self):
+    def test_post(self):
         request = self.factory.get('/api/proofs/', format='json', follow=True)
         request.user = self.user
         data = {   
                    "title": "title",
                    "prove": "prove",
                    "given": "given",
-                   #"diagram": None,
                    "plan": "plan",
                    "statements": [],
                    "reasons": [],
                }
         response = self.client.post('/api/proofs/',data, format='json', follow=True)
-        #response = ProofViewSet.as_view(request)
         self.assertEqual(response.status_code, 201)
         expected = data
-        expected['id'] =1
+        expected['id'] = 1
         expected['diagram']=None 
         returned = json.loads(response.content.decode(response.charset))
         self.assertEqual(expected, returned)
