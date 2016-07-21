35a02199820777cf7de655ba5d7f14917bea0341
rich-hart <rich.hart@rice.edu>
2016-07-21 13:06:51 -0500

    add user and group endpoint

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,14 @@
 from django.conf.urls import url, include
 from rest_framework import routers
-from .views import TheoremViewSet, AxiomViewSet, DefinitionViewSet
+from .views import (TheoremViewSet, 
+                    AxiomViewSet, 
+                    DefinitionViewSet,
+                    UserViewSet,
+                    GroupViewSet)
 
 router = routers.DefaultRouter()
-
+router.register(r'users',UserViewSet)
+router.register(r'groups',GroupViewSet)
 router.register(r'definitions', DefinitionViewSet)
 router.register(r'theorems', TheoremViewSet)
 router.register(r'axioms', AxiomViewSet)
--- api/views.py
+++ api/views.py
@@ -1,6 +1,26 @@
 from proof.models import Theorem, Axiom, Definition
+from django.contrib.auth.models import User, Group
 from rest_framework import viewsets
-from .serializers import TheoremSerializer, AxiomSerializer, DefinitionSerializer
+from .serializers import (TheoremSerializer, 
+                          AxiomSerializer, 
+                          DefinitionSerializer,
+                          UserSerializer, 
+                          GroupSerializer,)
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+
+
+class GroupViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows groups to be viewed or edited.
+    """
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
 
 class DefinitionViewSet(viewsets.ModelViewSet):
     queryset = Definition.objects.all()
