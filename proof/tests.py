788830aa6dd5e56b7dd4592810f2d0931d19296d
Rich Hart <Rich.Hart@rice.edu>
2016-08-09 18:15:07 -0500

    Update tests.py

--- proof/tests.py
+++ proof/tests.py
@@ -6,7 +6,6 @@ import yaml
 from .utils import book_to_json
 class Utils(TestCase):
     def test_book_to_json(self):
-        import ipdb; ipdb.set_trace()
         returned = book_to_json('data/test/book/geometry/')
         self.assertIsNone(returned) 
     
